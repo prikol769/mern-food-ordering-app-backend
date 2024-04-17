ceebc6677de1f6cdaba217f2a31c1328b8d888ec
prikol769 <prikol769@gmail.com>
2024-04-17 11:54:39 -0500

    added getRestaurant controller

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from "express";
 import Restaurant from "../models/restaurant";
 
+const getRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurantId = req.params.restaurantId;
+
+    const restaurant = await Restaurant.findById(restaurantId);
+
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant not found" });
+    }
+
+    res.json(restaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+};
+
 const searchRestaurants = async (req: Request, res: Response) => {
   try {
     const city = req.params.city;
@@ -78,4 +95,5 @@ const searchRestaurants = async (req: Request, res: Response) => {
 
 export default {
   searchRestaurants,
+  getRestaurant,
 };
